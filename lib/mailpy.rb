b334f4e8681c6f8d4f72fd553dbc2c1c8981a041
Reza Tafazzul Zaheer <reza.zaheer@binus.ac.id>
2025-09-24 09:01:15 +0700

    [Other] Refactor error handler for readability

--- lib/mailpy.rb
+++ lib/mailpy.rb
@@ -17,27 +17,26 @@ module Mailpy
     end
 
     private
+
     def perform_send_request(mail, settings)
       begin
         result = MailerApi.new(mail, settings).send
-    
-        unless result.code === 200
-          message = begin
-            JSON.parse(result.body)['message']
-          rescue JSON::ParserError
-            Rails.logger.error("Mailpy response is not JSON: #{result.body[0..300]}")
-            "Unexpected response from Mailpy: #{result.body[0..100]}"
-          end
-          raise(MailpyDeliveryError, message)
-        end
-
-        result
+        return result if result.code == 200
+        error_message = extract_error_message(result.body)
+        raise(MailpyDeliveryError, error_message)
       rescue Errno::ECONNREFUSED, Mailpy::DeliveryMethod::MailpyDeliveryError, Errno::ECONNRESET
         smtp_settings = Rails.application.config.action_mailer.smtp_settings
         raise(MailpyDeliveryError, "SMTP miss configured. Please add SMTP configuration in your environment config") if smtp_settings.blank?
         result = SMTPApi.new(mail, smtp_settings).send
         result
       end
+    end
+    
+    def extract_error_message(response_body)
+      JSON.parse(response_body)['message']
+    rescue JSON::ParserError
+      Rails.logger.error("Mailpy response is not JSON: #{response_body[0..300]}")
+      "Unexpected response from Mailpy: #{response_body[0..100]}"
     end
   end
 end
